258e9d9fe8c08c26f0ba4f5c9f1563e7f98252ea
Dev-edx <d.bhuptani@edexa.team>
2023-11-05 20:14:33 +0530

    feat: integrated worldcoin,dynamic,chains

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import List from './pages/list/List';
 import Hotel from './pages/hotel/Hotel';
 import Login from './pages/login/Login';
 import RegisterHotel from './pages/registerHotel/RegisterHotel';
+import { DynamicContextProvider, SortWallets } from '@dynamic-labs/sdk-react';
+import { useState } from 'react';
 
 function App() {
+  const [alertProps, setAlertProps] = useState({
+    show: false,
+    type: '',
+    message: '',
+  });
+  const [showDynamicNav, setShowDynamicNav] = useState(false);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/hotels" element={<List/>}/>
-        <Route path="/hotels/:id" element={<Hotel/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<RegisterHotel />}/>
-      </Routes>
-    </BrowserRouter>
+    <DynamicContextProvider
+      settings={{
+        environmentId: 'be72e05e-2712-4d85-8d2f-214df749013f',
+        walletsFilter: SortWallets([
+          'coinbase',
+          'metamask',
+          'walletconnect',
+          'zengo',
+        ]),
+        defaultNumberOfWalletsToShow: 4,
+        eventsCallbacks: {
+          onLinkSuccess: (args) => {
+            setAlertProps({
+              show: true,
+              type: 'success',
+              message: 'Wallet linked!',
+            });
+            setShowDynamicNav(false);
+          },
+          onAuthSuccess: (args) => {
+            setAlertProps({
+              show: true,
+              type: 'success',
+              message: 'Auth success!',
+            });
+          },
+          onLogout: (args) => {
+            setAlertProps({
+              show: true,
+              type: 'success',
+              message: 'Logout success!',
+            });
+          },
+          onUserProfileUpdate: (user) => {
+            setAlertProps({
+              show: true,
+              type: 'success',
+              message: 'Profile update success!',
+            });
+          },
+        },
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotels" element={<List />} />
+          <Route path="/hotels/:id" element={<Hotel />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegisterHotel />} />
+        </Routes>
+      </BrowserRouter>
+    </DynamicContextProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,17 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthContextProvider } from "./context/AuthContext";
-import { SearchContextProvider } from "./context/SearchContext";
+import { AuthContextProvider } from './context/AuthContext';
+import { SearchContextProvider } from './context/SearchContext';
+import { WagmiConfig, configureChains, createConfig } from 'wagmi';
+import { Web3Modal } from '@web3modal/react';
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from '@web3modal/ethereum';
+import {
+  arbitrum,
+  base,
+  celo,
+  filecoin,
+  mantle,
+  neonMainnet,
+  polygon,
+  scroll,
+  zkSync,
+} from 'viem/chains';
+
+const chains = [
+  zkSync,
+  polygon,
+  filecoin,
+  scroll,
+  arbitrum,
+  base,
+  celo,
+  mantle,
+  neonMainnet
+];
+
+// chiliz, aztec, near, cartesi
+const projectId = 'a6c1b0f365aaef1f0617e77730208a3e';
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <AuthContextProvider>
-      <SearchContextProvider>
-        <App />
-      </SearchContextProvider>
-    </AuthContextProvider>
+    <WagmiConfig config={wagmiConfig}>
+      <AuthContextProvider>
+        <SearchContextProvider>
+          <App />
+        </SearchContextProvider>
+      </AuthContextProvider>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </WagmiConfig>
   </>
 );
-
